919caa7d118d09e9975e80b169089a9ca29c2507
t.kaneko <nontan20xx@gmail.com>
2017-07-13 17:05:14 +0900

    Mod boot.asm

--- boot.asm
+++ boot.asm
@@ -3,11 +3,12 @@
 ORG	0x7C00
 
 JMP	BOOT
-BS_jmpBoot2	DB	0x90
+
 BS_OEMName	DB	"MyOS    "
 BPB_BytsPerSec	DW	0x0200
 BPB_SecPerClus	DB	0x01
 BPB_RsvdSecCnt	DW	0x0001
+BPB_NumFATs	DB	0x02
 BPB_RootEntCnt	DW	0x00E0
 BPB_TotSec16	DW	0x0B40
 BPB_Media	DB	0xF0
@@ -16,38 +17,13 @@ BPB_SecPerTrk	DW	0x0012
 BPB_NumHeads	DW	0x0002
 BPB_HiddSec	DD	0x00000000
 BPB_TotSec32	DD	0x00000000
+
 BS_DrvNum	DB	0x00
 BS_Reserved1	DB	0x00
 BS_BootSig	DB	0x29
 BS_VolID	DD	0x20170711
 BS_VolLab	DB	"MyOS       "
 BS_FilSysType	DB	"FAT12   "
-
-DisplayMessage:
-	PUSH	AX
-	PUSH	BX
-StartDispMsg:
-	LODSB
-	OR	AL, AL
-	JZ	.DONE
-	MOV	AH, 0x0E
-	MOV	BH, 0x00
-	MOV	BL, 0x07
-	INT	0x10
-	JMP	StartDispMsg
-.DONE:
-	POP	BX
-	POP	AX
-	RET
-
-ResetFloopyDrive:
-	MOV	AH, 0x00
-	MOV	DL, 0x00
-	INT	0x13
-	JC	FAILURE
-	HLT
-FAILURE:
-	HLT
 
 BOOT:
 	CLI
@@ -65,28 +41,70 @@ BOOT:
 	MOV	SS, AX
 	MOV	SP, 0xFFFC
 
-	MOV	AH, 0x0E
-	MOV	AL, 0x41
-	MOV	BH, 0x00
-	MOV	BL, 0x07
-	INT	0x10
-	MOV	AH, 0x0E
-	MOV	AL, 0x42
-	MOV	BH, 0x00
-	MOV	BL, 0x07
-	INT	0x10
-	MOV	AH, 0x0E
-	MOV	AL, 0x43
-	MOV	BH, 0x00
-	MOV	BL, 0x07
-	INT	0x10
+LOAD_FAT:
+	MOV	BX, WORD [BX_FAT_ADDR]
+	ADD	AX, WORD [BPB_RsvdSecCnt]
+	XCHG	AX, CX
+	MOV	AX, WORD [BPB_FATSz16]
+	MUL	WORD [BPB_NumFATs]
+	XCHG	AX, CX
 
-	ImageName	DB "Good-bye Small World", 0x00
+READ_FAT:
+	CALL	ReadSector
+	ADD	BX, WORD [BPB_BytsPerSec]
+	INC	AX
+	DEC	CX
+	JCXZ	FAT_LOADED
+	JMP	READ_FAT
 
-	MOV	SI, ImageName
-	CALL	DisplayMessage
-
+FAT_LOADED:
 	HLT
+
+ReadSector:
+	MOV	DI, 0x0005
+SECTORLOOP:
+	PUSH	AX
+	PUSH	BX
+	PUSH	CX
+	CALL	LBA2CHS
+	MOV	AH, 0x02
+	MOV	AL, 0x01
+	MOV	CH, BYTE [physicalTrack]
+	MOV	CL, BYTE [physicalSector]
+	MOV	DH, BYTE [physicalHead]
+	MOV	DL, BYTE [BS_DrvNum]
+	INT	0x13
+	JNC	SUCCESS
+	XOR	AX, AX
+	INT	0x13
+	DEC	DI
+	POP	CX
+	POP	BX
+	POP	AX
+	JNZ	SECTORLOOP
+	INT	0x18
+SUCCESS:
+	POP	CX
+	POP	BX
+	POP	AX
+	RET
+
+LBA2CHS:
+	XOR	DX, DX
+	DIV	WORD [BPB_SecPerTrk]
+	INC	DL
+	MOV	BYTE [physicalSector], DL
+	XOR	DX, DX
+	DIV	WORD [BPB_NumHeads]
+	MOV	BYTE [physicalHead], DL
+	MOV	BYTE [physicalTrack], AL
+	RET
+
+physicalSector	DB 0x00
+physicalHead	DB 0x00
+physicalTrack	DB 0x00
+
+BX_FAT_ADDR	DW 0x7E00
 
 TIMES 510 - ($ -$$) DB 0
 
