36bb8d24f4a10dbf58f1bb9744bcada0d8614495
t.kaneko <nontan20xx@gmail.com>
2017-08-01 20:20:39 +0900

    Mod starting.asm

--- src/starting.asm
+++ src/starting.asm
@@ -7,21 +7,20 @@ ORG 0x500
 
 
 ; Data Section 
-msgphello		DB 0x0D, 0x0A, "Hello", 0x0D, 0x0A, 0x00
-
+msgphello DB 0x0D, 0x0A, "Hello", 0x0D, 0x0A, 0x00
 
 ; Starting Kernel Procedure
 MAIN2:
 	; 汎用レジスタ初期化
-	XOR	AX, AX
-	XOR	BX, BX
-	XOR	CX, CX
-	XOR	DX, DX
+	XOR AX, AX
+	XOR BX, BX
+	XOR CX, CX
+	XOR DX, DX
 	; データセグメント初期化
-	MOV	DS, AX
-	MOV	ES, AX
+	MOV DS, AX
+	MOV ES, AX
 	; メッセージ表示
-	MOV	SI, msgphello
-	CALL	DisplayMessage
+	MOV SI, msgphello
+	CALL DisplayMessage
 	
 	HLT
